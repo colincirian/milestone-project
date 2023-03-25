d77a26d32a9074d1c151be115813def8b3f31784
Colin <colindcirian@gmail.com>
2023-03-24 23:33:49 -0700

    added some css and javascript

---
+++ project-1/index.js
@@ -0,0 +1,17 @@
+// start button 
+const startButton = document.querySelector('.js-start');
+
+function startGame() {
+    startButton.classList.add('hidden');
+}
+
+startButton.addEventListener('click', startGame)
+
+let sequence = [];
+let playerSequence =[];
+
+
+let 
+function moveCircles() {
+
+}
