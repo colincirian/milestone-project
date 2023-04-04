5958383c54f83f0acf0ff190fcbfc702cac84e38
Colin <colindcirian@gmail.com>
2023-04-03 21:39:07 -0700

    got my target to move and a timer

--- project-1/index.js
+++ project-1/index.js
@@ -1,42 +1,70 @@
 // function to each section
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById("start-button").addEventListener("click", function() {
-    document.getElementById("section2").scrollIntoView({ behavior: 'smooth' });
-  });
-})
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("start-button")
+    .addEventListener("click", function () {
+      document
+        .getElementById("section2")
+        .scrollIntoView({ behavior: "smooth" });
+    });
+});
 
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById("easyBtn").addEventListener("click", function() {
-    document.getElementById("section3").scrollIntoView({ behavior: 'smooth' });
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("easyBtn").addEventListener("click", function () {
+    document.getElementById("section3").scrollIntoView({ behavior: "smooth" });
   });
-})
+});
 
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById("intermediateBtn").addEventListener("click", function() {
-    document.getElementById("section4").scrollIntoView({ behavior: 'smooth' });
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("intermediateBtn")
+    .addEventListener("click", function () {
+      document
+        .getElementById("section4")
+        .scrollIntoView({ behavior: "smooth" });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("hardBtn").addEventListener("click", function () {
+    document.getElementById("section5").scrollIntoView({ behavior: "smooth" });
   });
-})
+});
 
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById("hardBtn").addEventListener("click", function() {
-    document.getElementById("section5").scrollIntoView({ behavior: 'smooth' });
-  });
-})
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   const homeBtn = document.querySelectorAll(".homeBtn");
-  
-  homeBtn.forEach(btn => {
-    btn.addEventListener("click", function() {
-      document.getElementById("section1").scrollIntoView({ behavior: 'smooth' });
+
+  homeBtn.forEach((btn) => {
+    btn.addEventListener("click", function () {
+      document
+        .getElementById("section1")
+        .scrollIntoView({ behavior: "smooth" });
     });
   });
 });
 
-
 // function that makes the element disappear when clicked
-document.addEventListener('DOMContentLoaded', function() {
-window.onload = function() {
+document.addEventListener("DOMContentLoaded", function () {
+  const timeDisplay = document.querySelector(".time");
+  const alert = document.querySelector("#easy-container");
+  let time = 45;
+  let score = 0;
+
+  // display time
+  timeDisplay.innerHTML = `time: ${time}`;
+
+  // timer countdown
+  let timer = setInterval(function () {
+    time--; // decrement time remaining
+    timeDisplay.innerHTML = `time: ${time}`;
+
+    if (time <= 0) {
+      clearInterval(timer); // stop timer
+      alert.innerHTML = "GAME OVER!";
+    }
+  }, 1000); // repeat every 1000ms (1 second)
+
+
   const container = document.getElementById("easy-container");
   const target = document.getElementById("target");
 
@@ -54,47 +82,15 @@ window.onload = function() {
     const targetWidth = target.offsetWidth;
     const targetHeight = target.offsetHeight;
 
-    target.style.left = Math.floor(Math.random() * (containerWidth - targetWidth)) + "px";
-    target.style.top = Math.floor(Math.random() * (containerHeight - targetHeight)) + "px";
+    target.style.left =
+      Math.floor(Math.random() * (containerWidth - targetWidth)) + "px";
+    target.style.top =
+      Math.floor(Math.random() * (containerHeight - targetHeight)) + "px";
   }
 
   spawnTarget();
 
   target.addEventListener("click", () => {
-    target.remove();
     spawnTarget();
-   });
-  };
-})
-
-
-
-
-
-
-
-
-
-
-/*
-// Define the square area where the element will spawn
-const spawnArea = {
-  x: 0, // left boundary
-  y: 0, // top boundary
-  width: 650, // horizontal size
-  height: 650, // vertical size
-};
-
-// Set up the random number generator
-const random = (min, max) => Math.random() * (max - min) + min;
-
-// Generate random coordinates within the spawn area
-const spawnX = random(spawnArea.x, spawnArea.x + spawnArea.width);
-const spawnY = random(spawnArea.y, spawnArea.y + spawnArea.height);
-
-// Create the element at the random coordinates
-const element = document.createElement("div");
-element.style.position = "absolute";
-element.style.left = spawnX + "px";
-element.style.top = spawnY + "px";
-document.body.appendChild(element); */
+  });
+});
